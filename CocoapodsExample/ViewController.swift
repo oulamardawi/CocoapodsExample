393fbdfea000773f8e0dcddf31a3e180c5af04b5
Mardawi <ola.mardawi@gmail.com>
2022-03-20 12:01:36 +0200

    Initial Commit

---
+++ CocoapodsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocoapodsExample
+//
+//  Created by Oula mardawi on 20/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
